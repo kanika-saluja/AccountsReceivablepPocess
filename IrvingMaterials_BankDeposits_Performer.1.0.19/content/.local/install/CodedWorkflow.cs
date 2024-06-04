036615d961fc3134da44bd6b2e9c40b3870563d8
kanika <kanika.saluja@centricconsulting.com>
2024-06-04 07:50:50 -0400

    api token changes to AR process as of june 4
    api token changes to AR process as of june 4

---
+++ IrvingMaterials_BankDeposits_Performer.1.0.19/content/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace IrvingMaterials_BankDeposits_Performer
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
